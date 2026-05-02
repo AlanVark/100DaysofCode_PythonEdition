ffb0a0efbe11b17c173325657ec0fbd3914a4ae0
AlanVark <104864732+AlanVark@users.noreply.github.com>
2026-05-02 15:45:05 -0700

    Update P1_BrandNameGen.py

--- P1_BrandNameGen.py
+++ P1_BrandNameGen.py
@@ -1,7 +1,5 @@
 print('Welcome to the Brand Name Generator')
-print('What\'s the name of the city you grew up in?')
-city = input()
-print('What\'s your pet\'s name?')
-pet_name = input()
+city = input('What\'s the name of the city you grew up in?')
+pet_name = input('What\'s your pet\'s name?')
 brand_name = city +" " + pet_name
 print(f"Your brand name is {brand_name}")
