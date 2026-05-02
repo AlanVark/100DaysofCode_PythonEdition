9de8596ac65adc1ecc992ae64d43fff48f0ddcb3
AlanVark <104864732+AlanVark@users.noreply.github.com>
2026-05-02 21:52:55 +0000

    Rename README, add BrandNameGen, and clean up files

---
+++ P1_BrandNameGen.py
@@ -0,0 +1,7 @@
+print('Welcome to the Brand Name Generator')
+print('What\'s the name of the city you grew up in?')
+city = input()
+print('What\'s your pet\'s name?')
+pet_name = input()
+brand_name = city +" " + pet_name
+print(f"Your brand name is {brand_name}")
